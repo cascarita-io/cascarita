19757ae4d511a7d78a8c39e877f68429dace98e9
garcialuis <garcialuisdev@gmail.com>
2025-02-03 12:35:01 -0800

    migration: update userroles table to add unique constrain between user and role

---
+++ server/migrations/20250203202920-add-unique-constraint-to-userroles.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint("UserRoles", {
+      fields: ["user_id", "role_id"],
+      type: "unique",
+      name: "unique_user_role",
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("UserRoles", "unique_user_role");
+  }
+};
