47cff9dfecef52ae9cbb11fe72c7a4a191983828
garcialuis <garcialuisdev@gmail.com>
2025-02-03 13:48:50 -0800

    migration: adds rolePermissions table and model

---
+++ server/migrations/20250203203552-create-role-permissions-table.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("RolePermissions", {
+      id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      role_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        references: {
+          model: "Roles",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      permission_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        references: {
+          model: "Permissions",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+      },
+    });
+
+    await queryInterface.addConstraint("RolePermissions", {
+      fields: ["role_id", "permission_id"],
+      type: "unique",
+      name: "unique_role_permission",
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable("RolePermissions");
+  }
+};
