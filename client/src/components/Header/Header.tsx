6d8066158e2a8dcd0227edf737172b814eeb65d6
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2025-02-06 17:07:32 -0800

    Added profile pic from auth0

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,12 @@
 import styles from "./Header.module.css";
 import { Text } from "@radix-ui/themes";
 import { Avatar } from "@radix-ui/themes";
-// import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import LanguagePreferenceButton from "../LanguagePreferenceButton/LanguagePreferenceButton";
 import cascaritaDefault from "../../assets/Logos/CascaritaLogo/cascarita_default.svg";
 
 const Header = () => {
-  //TODO: We need to import profile pic to place in Avatar component
-  // const { user } = useAuth0();
+  const { user } = useAuth0();
 
   return (
     <header className={styles.header}>
@@ -23,7 +22,7 @@ const Header = () => {
         <LanguagePreferenceButton className={styles.settingsBtn} />
 
         <Avatar
-          src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+          src={user && user.picture}
           fallback="A"
           radius="full"
           className={`${styles.settingsBtn} ${styles.avatar}`}
