c55cd1fae74cfb3c83ad3f3110a585381af53bac
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2025-02-07 16:19:39 -0800

    Added styling to Table data

--- client/src/pages/Users/Users.tsx
+++ client/src/pages/Users/Users.tsx
@@ -1,3 +1,6 @@
+// TODO: Remove when done testing
+//@ts-ignore
+
 // Libraries
 import { useTranslation } from "react-i18next";
 import * as Avatar from "@radix-ui/react-avatar";
@@ -10,7 +13,8 @@ import { fetchUser, getUsersByGroupId } from "../../api/users/service";
 // Components
 // import { useAuth } from "../../components/AuthContext/AuthContext";
 import Page from "../../components/Page/Page";
-import styles from "./Users.module.css";
+import styles from "../pages.module.css";
+import pagesStyles from "../pages.module.css";
 import DashboardTable from "../../components/DashboardTable/DashboardTable";
 import DropdownMenuButton from "../../components/DropdownMenuButton/DropdownMenuButton";
 import { User } from "./types";
@@ -20,13 +24,7 @@ import Modal from "../../components/Modal/Modal";
 import UserForm from "../../components/Forms/UserForm/UserForm";
 import { useAuth0 } from "@auth0/auth0-react";
 import Cookies from "js-cookie";
-
-const mapRoles = (role_id: number) => {
-  switch (role_id) {
-    case 1:
-      return "Staff";
-  }
-};
+import { FaPlus } from "react-icons/fa";
 
 const Users = () => {
   // Confligure translation
@@ -94,10 +92,6 @@ const Users = () => {
     }
   }, [users, debouncedQuery]);
 
-  const handleAddUser = () => {
-    setIsAddUserOpen(true);
-  };
-
   const handleEditUser = (user: User) => {
     setSelectedUser(user);
     setIsEditOpen(true);
@@ -114,16 +108,31 @@ const Users = () => {
         <div className={styles.dropdown}>
           <Search onSearchChange={setSearchQuery} />
         </div>
-        <PrimaryButton onClick={handleAddUser}>{t("addUser")}</PrimaryButton>
+        <Modal open={isAddUserOpen} onOpenChange={setIsAddUserOpen}>
+          <Modal.Button asChild className={pagesStyles.modalTrigger}>
+            <PrimaryButton
+              className={pagesStyles.primaryBtn}
+              onClick={() => setIsAddUserOpen(true)}
+            >
+              <p className={pagesStyles.btnTextDesktop}>{t("addUser")}</p>
+              <FaPlus className={pagesStyles.btnTextMobile} />
+            </PrimaryButton>
+          </Modal.Button>
+          <Modal.Content title="Add User">
+            <UserForm
+              afterSave={() => setIsAddUserOpen(false)}
+              requestType="POST"
+              selectedUserId={selectedUser?.id}
+              parentUserGroupId={groupId}
+            />
+          </Modal.Content>
+        </Modal>
       </div>
 
       {filteredUsers == null || filteredUsers.length == 0 ? (
         <p className={styles.noUsersMessage}>No users to display...</p>
       ) : (
-        <DashboardTable
-          headers={["Name", "Email", "Role", "Options"]}
-          headerColor="light"
-        >
+        <DashboardTable headers={["Name", "Options"]} headerColor="light">
           {isLoading ? (
             <tr>
               <td>Loading...</td>
@@ -133,9 +142,9 @@ const Users = () => {
               <td>Error Fetching Data</td>
             </tr>
           ) : (
-            filteredUsers?.map((user: User, idx: number) => (
-              <tr key={idx} className={styles.tableRow}>
-                <td className={`${styles.tableData} ${styles.leadingColumn}`}>
+            mockUsers?.map((user) => (
+              <tr key={user.id} className={styles.tableRow}>
+                <td className={styles.tableData}>
                   <Avatar.Root className="AvatarRoot">
                     <Avatar.Image
                       className={styles.avatar}
@@ -147,15 +156,15 @@ const Users = () => {
                         user.last_name.charAt(0).toUpperCase}
                     </Avatar.Fallback>
                   </Avatar.Root>
-                  <div className={styles.email}>
-                    {user.first_name} {user.last_name}
+                  <div style={{ display: "grid" }}>
+                    <p>{`${user.first_name} ${user.last_name}`}</p>
+                    <p style={{ fontSize: "0.7rem" }}>{`${user.role}`}</p>
                   </div>
                 </td>
-                <td className={styles.tableData}>{user.email}</td>
-                <td className={styles.tableData}>{mapRoles(user.role_id)}</td>
                 <td className={styles.tableData}>
                   <DropdownMenuButton>
                     <DropdownMenuButton.Item
+                      // @ts-ignore
                       onClick={() => handleEditUser(user)}
                     >
                       Edit
@@ -166,6 +175,7 @@ const Users = () => {
                     />
 
                     <DropdownMenuButton.Item
+                      // @ts-ignore
                       onClick={() => handleDeleteUser(user)}
                     >
                       Delete
@@ -178,16 +188,6 @@ const Users = () => {
         </DashboardTable>
       )}
 
-      <Modal open={isAddUserOpen} onOpenChange={setIsAddUserOpen}>
-        <Modal.Content title="Add User">
-          <UserForm
-            afterSave={() => setIsAddUserOpen(false)}
-            requestType="POST"
-            selectedUserId={selectedUser?.id}
-            parentUserGroupId={groupId}
-          />
-        </Modal.Content>
-      </Modal>
       <Modal open={isEditOpen} onOpenChange={setIsEditOpen}>
         <Modal.Content
           title={`Edit ${selectedUser ? formatName(selectedUser) : ""}`}
@@ -214,5 +214,99 @@ const Users = () => {
     </Page>
   );
 };
+
+const mockUsers = [
+  {
+    id: 1,
+    first_name: "Kelly",
+    last_name: "Frazer",
+    email: "kfrazer0@patch.com",
+    role: "Food Chemist",
+  },
+  {
+    id: 2,
+    first_name: "Felix",
+    last_name: "Magnay",
+    email: "fmagnay1@bloglines.com",
+    role: "Systems Administrator III",
+  },
+  {
+    id: 3,
+    first_name: "Nicolis",
+    last_name: "McNirlan",
+    email: "nmcnirlan2@google.de",
+    role: "Electrical Engineer",
+  },
+  {
+    id: 4,
+    first_name: "Miltie",
+    last_name: "Monelle",
+    email: "mmonelle3@e-recht24.de",
+    role: "Technical Writer",
+  },
+  {
+    id: 5,
+    first_name: "Benedetto",
+    last_name: "Finder",
+    email: "bfinder4@ameblo.jp",
+    role: "Senior Cost Accountant",
+  },
+  {
+    id: 6,
+    first_name: "Gaynor",
+    last_name: "Hehir",
+    email: "ghehir5@imdb.com",
+    role: "Accountant I",
+  },
+  {
+    id: 7,
+    first_name: "Madelina",
+    last_name: "Grebbin",
+    email: "mgrebbin6@amazon.com",
+    role: "Product Engineer",
+  },
+  {
+    id: 8,
+    first_name: "Sherri",
+    last_name: "Assandri",
+    email: "sassandri7@cdc.gov",
+    role: "Community Outreach Specialist",
+  },
+  {
+    id: 9,
+    first_name: "Hale",
+    last_name: "Cannon",
+    email: "hcannon8@deviantart.com",
+    role: "Senior Cost Accountant",
+  },
+  {
+    id: 10,
+    first_name: "Levi",
+    last_name: "Auchinleck",
+    email: "lauchinleck9@sina.com.cn",
+    role: "Speech Pathologist",
+  },
+  {
+    id: 11,
+    first_name: "Albina",
+    last_name: "Abba",
+    email: "aabbaa@technorati.com",
+    role: "GIS Technical Architect",
+  },
+  {
+    id: 12,
+    first_name: "Sallyanne",
+    last_name: "Emanueli",
+    email: "semanuelib@home.pl",
+    role: "Software Test Engineer I",
+  },
+  {
+    id: 13,
+    first_name: "Nessy",
+    last_name: "Winspear",
+    email: "nwinspearc@google.com",
+    role: "Human Resources Assistant III",
+  },
+];
 
 export default Users;
