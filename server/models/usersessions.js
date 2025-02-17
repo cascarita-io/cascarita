ed2b615e16e4f9357b44efb5e8d9e21d0bfe675a
Jesus Gomez <40707209+JesusGo115@users.noreply.github.com>
2025-02-16 19:23:06 -0800

    User and Form table Updates and new UserSessions Table (#184)

---
+++ server/models/usersessions.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class UserSessions extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      UserSessions.belongsTo(models.User, {
+        foreignKey: "user_id",
+        targetKey: "id",
+      });
+      UserSessions.belongsTo(models.Session, {
+        foreignKey: "session_id",
+        targetKey: "id",
+      });
+    }
+  }
+  UserSessions.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      session_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "UserSessions",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  );
+  return UserSessions;
+};
