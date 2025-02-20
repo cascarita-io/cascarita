e0e89e6a91dd14f6cca1f5bebbdca9db5940d34e
Juan Ramirez <contact.juanmramirez@gmail.com>
2025-02-19 21:22:52 -0800

    Simple component layout

--- client/src/components/ThankYou/ThankYou.tsx
+++ client/src/components/ThankYou/ThankYou.tsx
@@ -1,7 +1,11 @@
-export default function ThankYou() {
+import React from "react";
+
+const ThankYou: React.FC = () => {
   return (
-    <div>
-      <h1>TESTING TY Component</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-8">
+      <h1 className="text-2xl font-bold text-gray-800">Thank You!</h1>
     </div>
   );
-}
+};
+
+export default ThankYou;
