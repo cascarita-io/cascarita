b00a6dda6d884db03e05c7852a06f6b56dd8ada8
Armando Bañuelos <32311654+abanuelo@users.noreply.github.com>
2025-02-23 23:50:51 -0800

    ops: terraform multicontainer deployment (#177)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,13 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     host: true,
-    port: 80,
-    proxy: {
-      "/api": {
-        target: "http://server:3000", // Traefik's route for the backend
-        changeOrigin: true,
-      },
-    },
+    port: 8080,
   },
 
   plugins: [tsconfigPaths(), react()],
