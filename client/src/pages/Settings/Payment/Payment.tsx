9c575e150a09d50b9948cbe36f95faf1311b2c06
Armando Banuelos <abanuelo@stanford.edu>
2025-02-28 00:01:10 -0800

    fix: eslint

--- client/src/pages/Settings/Payment/Payment.tsx
+++ client/src/pages/Settings/Payment/Payment.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "../Payment/Payment.module.css";
 import tableStyles from "../../pages.module.css";
 
