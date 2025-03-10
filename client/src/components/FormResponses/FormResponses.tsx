4ed749ed4a2cb14069fe0391eec31d9f1f5a8d42
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2025-03-10 13:15:02 -0700

    nit: removed stray console log

--- client/src/components/FormResponses/FormResponses.tsx
+++ client/src/components/FormResponses/FormResponses.tsx
@@ -95,6 +95,7 @@ const FormResponses = ({ formId }: FormResponsesProps) => {
   );
   const adminEmail = Cookies.get("email") || "";
   const [formDocumentId, setFormDocumentId] = useState("");
+  console.log(formDocumentId);
   const { t } = useTranslation("FormResponses");
 
   const formatDate = (dateString: string, daysAhead: number = 0): string => {
