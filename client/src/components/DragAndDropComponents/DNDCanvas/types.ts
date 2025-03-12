afe5c49356f2260e8fe6b3979f334cd9c8a03e80
Armando Bañuelos <32311654+abanuelo@users.noreply.github.com>
2025-03-12 08:40:47 -0700

    fix: prevent delete payment block (#243)

--- client/src/components/DragAndDropComponents/DNDCanvas/types.ts
+++ client/src/components/DragAndDropComponents/DNDCanvas/types.ts
@@ -3,6 +3,7 @@ import { DroppedItem } from "../../../pages/NewForm/types";
 
 export interface DNDCanvasProps {
   items: DroppedItem[];
+  formId: string | undefined;
   importedFields?: Field[];
   handleDelete: (name: string) => void;
   handleCopy: (index: number, copiedItem: DroppedItem) => void;
